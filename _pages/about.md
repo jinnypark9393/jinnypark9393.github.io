1241461ea8acf144d47dbabcbdf753d944a768eb
jinnypark9393 <jinnypark9393@gmail.com>
2022-04-14 05:31:14 -0700

    220414 21:30 Create _pages, _posts directory & Upload first blog post

---
+++ _pages/about.md
@@ -0,0 +1,13 @@
+---
+permalink: /about/
+title: "About"
+toc: true
+toc_sticky: true
+toc_label: "MYSELF"
+---
+
+![icon](/assets/images/logo.jepg)
+
+# 기본정보
+
+어쩌다 데브옵스 엔지니어가 된 문과생의 기록용 블로그(현재 블로그 이전 후 테스트 중)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "첫 글",
+        "excerpt":"깃허브 블로그 첫 글  우여곡절 끝에 깃허브 블로그를 생성했다. Jekyll 설치 문제 때문에 임시로 티스토리를 사용했었는데, 조만간 포스팅은 전부 이관할 예정이다. 이후 티스토리 블로그를 계속 운영할 지 여부는 고민이 더 필요할 듯 하다.  ","categories": ["ETC"],
+        "tags": ["GithubPages","깃허브페이지","깃허브블로그","개발자블로그비교"],
+        "url": "/etc/my-first-post/",
+        "teaser": null
+      }]
