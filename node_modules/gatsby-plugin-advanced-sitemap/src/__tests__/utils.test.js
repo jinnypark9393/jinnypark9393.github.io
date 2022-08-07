6276d7f0813bfe4347e279d13a5a75abcef0a909
Jinny Park <jinnypark9393@gmail.com>
2022-08-07 18:08:38 +0900

    220807 18:08 Jinny Removed cached files

--- node_modules/gatsby-plugin-advanced-sitemap/src/__tests__/utils.test.js
+++
@@ -1,7 +0,0 @@
-import * as utils from '../utils';
-
-describe(`It should test utils`, () => {
-    it(`should match the getDeclarations snapshot`, () => {
-        expect(utils.sitemapsUtils.getDeclarations()).toMatchSnapshot();
-    });
-});
