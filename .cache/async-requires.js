caedc8a580fb8fc016421fa526c0699bcf4132a4
Jinny Park <jinnypark9393@gmail.com>
2022-08-07 13:30:43 +0900

    220807 13:30 Jinny Remove unnecessary files related to gatsby build result

--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": () => import("./../../caches/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---cache-caches-gatsby-plugin-offline-app-shell-js" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-templates-blog-template-js": () => import("./../../../src/templates/blog-template.js" /* webpackChunkName: "component---src-templates-blog-template-js" */),
-  "component---src-templates-category-template-js": () => import("./../../../src/templates/category-template.js" /* webpackChunkName: "component---src-templates-category-template-js" */)
-}
-
--- public/component---cache-caches-gatsby-plugin-offline-app-shell-js-27c745220c51ea3f9945.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkjinnypark9393=self.webpackChunkjinnypark9393||[]).push([[306],{4284:function(e,n,t){var u=t(4836);n.__esModule=!0,n.default=void 0;var r=u(t(7867)),a=u(t(7294)),l=function(e){function n(){return e.apply(this,arguments)||this}return(0,r.default)(n,e),n.prototype.render=function(){return a.default.createElement(a.default.Fragment,null)},n}(a.default.Component),f=l;n.default=f}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-27c745220c51ea3f9945.js.map
